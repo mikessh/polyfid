d0fae9af224d438bab6cb5cc8059e74c1c52ff2e
mikessh <mikhail.shugay@gmail.com>
2016-09-23 01:19:32 +0300

    Update processing and analysis

--- processing/analyze.sh
+++ processing/analyze.sh
@@ -1,4 +1,4 @@
-for i in polerr2016-1 polerr2016-2
+for i in polerr73 polerr82 polerr2016-1 polerr2016-2
 do
    java -Xmx60G -jar mageri-1.0.0.jar -I $i.json --import-preset preset.xml -O $i-out/ 2>&1 | tee $i-log.txt
 done
